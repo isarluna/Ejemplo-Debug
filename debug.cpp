592ace62f11dc6170215b416cd5958c67ac5c4e6
Israel Argueta Luna <isarargueta@gmail.com>
2018-11-21 01:27:36 -0600

    Programa C++

---
+++ debug.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int suma=0;
+	for(int i=0;i<5;i++){
+		suma+=i;
+	}
+	cout<<"la suma es: "<<suma<<endl;
+	return 0;
+}
